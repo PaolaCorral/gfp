5693c854b02cb24e9d0c23a493acad7449502682
Paola <sm13ma.paolacorral@gmail.com>
2019-06-27 22:31:27 -0500

    Primer Login
    Login con firebase, aun no redirige a la pagina de tab1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,15 @@ export const environment = {
   production: false
 };
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyAmtHiXEjxpAPJjmdo3k68LY9AS2oWlBvk",
+  authDomain: "finanzaspersonales-1de63.firebaseapp.com",
+  databaseURL: "https://finanzaspersonales-1de63.firebaseio.com",
+  projectId: 'finanzaspersonales-1de63',
+  storageBucket: "",
+  messagingSenderId: "603283079257",
+  appId: "1:603283079257:web:c00453255660b235"
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
